aff278d7d5756a73226fdcbd0cfabe7b2274620a
rayan <mohamed.BOUDIBA@etu.univ-amu.fr>
2023-03-30 00:28:37 +0200

    add folder and dependencies

---
+++ controller/serverside.js
@@ -0,0 +1,315 @@
+"use strict"
+//acces restreint 
+//const Sqlite = require('better-sqlite3');
+const bcrypt = require('bcrypt');
+let fs = require('fs');
+//let db = new Sqlite('db.sqlite');
+const axios = require("axios");
+//cookie
+const cookieSession = require('cookie-session');
+
+
+
+/* Serveur pour le site de recettes */
+var express = require('express');
+var mustache = require('mustache-express');
+
+var model = require('../model/model');
+var app = express();
+
+
+// parse form arguments in POST requests
+const bodyParser = require('body-parser');
+const { rawListeners, title } = require('process');
+const { Console } = require('console');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine('html', mustache());
+app.set('view engine', 'html');
+app.set('views', '../view');
+
+app.use(cookieSession({
+  secret: 'mot-de-passe-du-cookie',
+}));
+app.use(authenticated);
+//app.use(change_header);
+
+function is_authenticated(req, res, next) {
+ 
+  if (!req.session.userid) {
+   // res.redirect('/not_authenticated')
+    return res.redirect('/login')
+    
+  }
+  
+  next();
+}
+function authenticated(req, res, next) {
+  if(req.session.userid && req.session){
+    res.locals.authenticated =true;
+    res.locals.name = req.session.name;
+   
+  }else{
+    // la session n'est pas valide
+    res.locals.authenticated = false;
+  }
+ 
+  next();
+}
+/**** Routes pour voir les pages du site ****/
+
+/* Retourne une page principale avec le nombre de recettes */
+app.get('/', (req, res) => {
+  var found;
+  var foundpc;
+  model.responseApi2(model.getGamesByPlateform('pc')) 
+  .then(function(response2) {
+    //found = { games: response2 };
+    //res.render('index', foundpc );
+    model.responseApi2(model.getTop10()) 
+    .then(function(response) {
+      var firstGame = response.shift();
+      found = { game: response, firstGame: firstGame ,games:response2};
+      res.render('index', found );
+    });
+
+  });
+
+   
+
+});
+
+
+
+
+
+/* Retourne les résultats de la recherche à partir de la requête "query" */
+app.get('/search', (req, res) => {
+   
+    var results
+  
+
+  console.log(req.query.query);
+  model.responseApiSearchBar( req.query.query,model.GetGameList())
+  .then(function(response) {
+    var found={results : results=response}
+    //console.log(response)
+    res.render('search',found )
+   
+  });
+  // Example usage
+  
+  //console.log(found)
+ 
+  
+});
+
+app.get('/catalog/:tag/:plateform', (req, res) => {
+   
+    var results
+  
+  
+  //console.log(toString(req.params.plateform));
+  console.log("Plateform: "+req.params.plateform);
+  const plat=toString(req.params.plateform);
+  console.log(plat)
+
+  model.responseApi2(model.GetGamesByTag(req.params.tag,req.params.plateform)) 
+  .then(function(response) {
+    
+    var found={results : results=response}
+    //console.log(response)
+    res.render('catalog',found )
+   
+  });
+
+  // Example usage
+  
+  //console.log(found)
+
+ 
+  
+});
+
+
+
+app.get('/read/:id', (req, res) => {
+  var entry;
+
+  model.getGameDetails(req.params.id)
+    .then(function(response) {
+      const gameExistsPromise = model.checkIfGameIsInFavorites(req.session.userid, response.id);
+
+      // Wait for both Promises to be resolved before executing the then() block
+      Promise.all([gameExistsPromise])
+        .then(function([gameExists]) {
+          entry = {id: response.id, title: response.title, description: response.description, game_url: response.game_url, thumbnail: response.thumbnail,short_description: response.short_description ,gameExists: gameExists};
+          console.log("Boolean " + gameExists);
+
+          res.render('read', entry);
+        })
+        .catch(function(error) {
+          console.log(error);
+          res.sendStatus(500);
+        });
+    })
+    .catch(function(error) {
+      console.log(error);
+      res.sendStatus(500);
+    });
+});
+
+app.get('/catalogFlash',is_authenticated,(req,res) =>{
+      var results;
+      var data= model.getFlashGamesList()
+      console.log(data);
+      var entry = {results:results=data}
+      res.render('catalogFlash',entry);
+    
+    //res.render('catalogFlash');
+});
+
+app.get('/play/:id',is_authenticated, (req, res) => {
+  var entry;
+  var data= model.getFlashGames(req.params.id)
+  res.render('play',data);
+});
+
+
+app.get('/favorite',is_authenticated, (req, res) => {
+  //var entry = model.read(req.params.id);
+ 
+  res.render('favorite');
+});
+  
+app.get('/update/:id',is_authenticated, (req, res) => {
+  var entry = model.read(req.params.id);
+  //res.render('update',{name:  req.session.name });
+  res.render('update', entry);
+});
+
+app.get('/delete/:id', is_authenticated,(req, res) => {
+  var entry = model.read(req.params.id);
+  //res.render('delete',{name:  req.session.name });
+  res.render('delete', {id: req.params.id, title: entry.title});
+});
+
+
+//formulaire login
+app.get('/login', (req, res) => {
+  
+  res.render('login');
+});
+//fail one
+app.get('/loginFail', (req, res) => {
+
+  res.render('login',{fail:  'wrong password or username' });
+});
+
+
+//page pour le forumulaire de new user
+app.get('/new_user', (req, res) => {
+  res.render('new_user');
+});
+
+//récupération des données 
+app.post('/login', (req, res) => {
+  const { name, password } = req.body;
+
+  // Vérifier si les champs sont renseignés
+  if (!name || !password) {
+    return res.redirect('/login?error=missing');
+  }
+  let result =model.checkLoginInput(name,password)
+  //console.log(result.id);
+  if(result != null ){
+    
+    req.session.userid= result.id;
+    req.session.name=result.name;
+    //console.log( "USER ID"+req.session.userid)
+    res.redirect('/');
+  }else{
+    res.redirect('/loginFail');
+    //res.redirect('/login');
+  }
+
+  
+
+});
+//logOutPost
+app.post('/logout', (req, res) => {
+  req.session.userid = null; // Destroys the current session
+  req.session.name='';
+  res.redirect('/'); // Redirects the user to the homepage
+});
+
+app.post('/new_user', (req, res) => {
+  const { name, password } = req.body;
+  // Vérifier si les champs sont renseignés
+  if (!name || !password) {
+    return res.redirect('/login?error=missing');
+  }
+
+  let result = model.new_user(name,password);
+  if(result ==3){
+    res.redirect('/new_user?error=duplicate');
+    //res.redirect('/'); // Redirects the user to the homepage
+  }else if(result !=null){
+    res.redirect('/'); // Redirects the user to the homepage
+   
+  }
+  res.redirect('/new_user?error=duplicate');
+});
+
+
+app.post('/add_favorite', async (req, res) => {
+  const { idgame, title, thumbnail, description } = req.body;
+  console.log(idgame + title +thumbnail +description );
+
+  try {
+    const isAlreadyInFavorites = await model.checkIfGameIsInFavorites(req.session.userid, idgame);
+    if (isAlreadyInFavorites) {
+      console.log("Game already in favorites");
+      //alert("game already in favorite");
+    } else {
+       model.addtofavorite(req.session.userid, idgame, thumbnail, description, title);
+      console.log("Game added to favorites");
+      //alert("game add to your favorite");
+    }
+  } catch (error) {
+    console.log("Error adding game to favorites: ", error);
+  }
+
+  res.redirect('/read/' + idgame);
+});
+
+
+
+
+/**** Routes pour modifier les données ****/
+
+// Fonction qui facilite la création d'une recette
+
+
+
+app.post('/update/:id', (req, res) => {
+  var id = req.params.id;
+  model.update(id, post_data_to_recipe(req));
+  res.redirect('/read/' + id);
+});
+
+app.post('/delete/:id', (req, res) => {
+  model.delete(req.params.id);
+  res.redirect('/');            
+});
+
+/* fonction permettant de gérer la création d'un utilisateur */
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
+
+
+
+
+
+
+ 
---
+++ model/model.js
@@ -0,0 +1,274 @@
+"use strict";
+const bcrypt = require('bcrypt');
+const axios = require("axios");
+const Sqlite = require('better-sqlite3');
+let db = new Sqlite('db.sqlite');
+
+
+
+//create new user
+function new_user(name, password){
+  // Generate a salt
+  const saltRounds = 10;
+  const salt = bcrypt.genSaltSync(saltRounds);
+  // Hash a password
+  const hash = bcrypt.hashSync(password, salt);
+
+  try {
+    var insert = db.prepare('INSERT INTO users (name,mdp,datecrea,dateupdate,state) VALUES (?,?,?,?,?);').run(name, hash, getnowDate(), getnowDate(), 1);
+    // If the insert succeeded, return the ID of the newly created user
+    return insert.lastInsertRowid;
+  } catch (error) {
+    // If the error is a unique constraint violation, return 3
+    if (error.message.includes("UNIQUE constraint failed")) {
+      return 3;
+    } else {
+      // If the error is not a unique constraint violation, log the error and return null
+      console.error(error);
+      return null;
+    }
+  }
+}
+
+function checkLoginInput(name,password){
+  var coordonne= db.prepare('SELECT * FROM users where name=? ').get(name);
+  
+     if(coordonne && bcrypt.compareSync(password, coordonne.mdp)){
+        console.log(coordonne);
+       return coordonne;
+     }
+     console.log(coordonne);
+     return null;
+      
+}
+function addtofavorite(iduser,idgame,thumbnail,description,title){
+  try{
+    var insert = db.prepare('INSERT INTO favorite (iduser,idgame,thumbnail,description,title) VALUES (?,?,?,?,?);').run(iduser,idgame,thumbnail,description,title);
+  }catch(error){
+    if (error.message.includes("UNIQUE constraint failed")) {
+      return 3;
+    } else {
+      // If the error is not a unique constraint violation, log the error and return null
+      console.error(error);
+      return null;
+    }
+  }
+ 
+
+}
+
+async function checkIfGameIsInFavorites(userId, gameId) {
+  const sql = 'SELECT * FROM favorite WHERE iduser = ? AND idgame = ?';
+  const stmt = db.prepare(sql);
+  const rows = await stmt.all(userId, gameId);
+  return rows.length > 0;
+}
+
+function getFlashGames(idgame){
+  var results = db.prepare('SELECT * from GAME where id = ?').get(idgame);
+  return results;
+  console.log(results);
+}
+
+function getFlashGamesList() {
+  // Assuming the db object is properly defined and connected
+  var statement = 'SELECT * FROM GAME';
+  var results = db.prepare(statement).all();
+  //console.log(results);
+  return results;
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////
+
+let options =null;
+
+function GetGamesByTag(tag,support){
+   return options = {
+        method: 'GET',
+        url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+        params: {platform: support, category: tag, 'sort-by': 'release-date'},
+        headers: {
+          'X-RapidAPI-Key': 'b5baf0e861msh5b61bf7a02b095ep133201jsn281e8c3c8a35',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+}
+
+function getGamesByPlateform(support){
+  return options = {
+       method: 'GET',
+       url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+       params: {platform: support, 'sort-by': 'release-date'},
+       headers: {
+         'X-RapidAPI-Key': 'b5baf0e861msh5b61bf7a02b095ep133201jsn281e8c3c8a35',
+         'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+       }
+   };
+}
+
+
+function getTop10(popularity){
+   return options = {
+        method: 'GET',
+        url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+        params: { 'sort-by': 'popularity'},
+        headers: {
+          'X-RapidAPI-Key': 'b5baf0e861msh5b61bf7a02b095ep133201jsn281e8c3c8a35',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+}
+
+
+async function getGameDetails(id){
+    const myKey = 'b5baf0e861msh5b61bf7a02b095ep133201jsn281e8c3c8a35';
+
+    const response = await fetch(
+      `https://www.freetogame.com/api/game?id=${id}`,
+      { cache: "no-cache" }
+    )
+    const data = await response.json();
+    //console.log('/////////////////////////////////////////////////////////////////////////////////////////////////////////////////');
+    //console.log(data);
+    return data;
+}
+
+
+
+
+
+
+  // Example usage
+  
+  
+
+
+
+
+
+
+function GetGameList(){
+    return options = {
+        method: 'GET',
+        url: 'https://free-to-play-games-database.p.rapidapi.com/api/games',
+        headers: {
+          'X-RapidAPI-Key': 'b5baf0e861msh5b61bf7a02b095ep133201jsn281e8c3c8a35',
+          'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+}
+
+
+
+module.exports = {
+    GetGamesByTag,
+    getGameDetails,
+    responseApiSearchBar,
+    getTop10,
+    GetGameList,
+    responseApi2,
+    getGameDetails,
+    getGamesByPlateform,
+    new_user,
+    checkLoginInput,
+    addtofavorite,
+    checkIfGameIsInFavorites,
+    getFlashGamesList,
+    getFlashGames
+
+};
+
+
+function responseApi2(options) {
+  const excludedGenres = ['Shooter', 'MOBA', 'mmofps', 'mmotps', 'mmorts', 'horror', 'ARPG', 'MMORPG'];
+  return axios.request(options).then(function(response) {
+    // filter
+    let filteredData = response.data.filter(obj => {
+      return obj.genre && 
+             !excludedGenres.some(g => obj.genre.includes(g)) && 
+             obj.title !== "Epic Cards Battle" && 
+             obj.title !== "5Street"
+             && obj.title !="Deathverse: Let It Die"
+             ;
+    });
+
+    console.log(filteredData);
+    return filteredData.slice(0, response.data.length);
+  }).catch(function(error) {
+    console.error(error);
+  });
+}
+
+
+
+
+
+function responseApiSearchBar(title, options) {
+  const list = [];
+  const excludedGenres = ['Shooter', 'MOBA', 'mmofps', 'mmotps', 'mmorts', 'horror','ARPG', 'MMORPG'];
+  const capitalizedWord = title.toUpperCase();
+
+  return axios.request(options).then(function (response) {
+    const filteredData = response.data.filter(obj => {
+      return obj.genre && !excludedGenres.some(g => obj.genre.includes(g));
+    });
+
+    filteredData.forEach(obj => {
+      if (obj.title.toUpperCase().includes(capitalizedWord) && obj.title !="Epic Cards Battle" && obj.title !="5Street"&& obj.title !="Deathverse: Let It Die") {
+        console.log(obj)
+        list.push(obj);
+      }
+    });
+
+    return list;
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
+
+
+
+
+//responseApi2(getGamesByPlateform('pc'));
+
+//responseApi2(GetGamesByTag('mmorpg','pc'))
+//responseApiSearchBar("Valo",GetGameList());
+
+
+//getGameDetails(5)
+//GetGamesByTag('mmorpg','browser');
+//responseApi2(GetGamesByTag('mmorpg','browser'));
+
+//GetGameList();
+
+//console.log(responseApi2(10,getTop10()));
+//console.log(responseApi3(2, {url: 'https://jsonplaceholder.typicode.com/todos'}))
+
+
+
+
+
+//const axios = require("axios");
+
+//const axios = require("axios");
+console.log(Date.now())
+function getnowDate(){
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = ("0" + (now.getMonth() + 1)).slice(-2);
+  const day = ("0" + now.getDate()).slice(-2);
+  const hours = ("0" + now.getHours()).slice(-2);
+  const minutes = ("0" + now.getMinutes()).slice(-2);
+  const seconds = ("0" + now.getSeconds()).slice(-2);
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  return formattedDate;
+  console.log(formattedDate);
+} 
+
+
+//console.log(getnowDate());
+
+//getFlashGamesList();
